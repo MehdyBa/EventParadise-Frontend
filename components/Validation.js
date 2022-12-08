448424f15fe7b95c8b73fa45dece4fc689b6c59a
mehdy <mehdy.badaoui07@gmail.com>
2022-12-08 17:08:33 +0100

    why?

--- components/Validation.js
+++ components/Validation.js
@@ -1,5 +1,5 @@
 
-const input = document.querySelector('input')
+/*const input = document.querySelector('input')
 const log = document.getElementById('log')
 
 input.addEventListener('invalid', (e) => {
@@ -7,4 +7,14 @@ input.addEventListener('invalid', (e) => {
     document.createElement('li'),
     { textContent: JSON.stringify(e.target.value) }
   ))
-})
+})*/
+
+const toastTrigger = document.getElementById('liveToastBtn')
+const toastLiveExample = document.getElementById('liveToast')
+if (toastTrigger) {
+  toastTrigger.addEventListener('click', () => {
+    const toast = new bootstrap.Toast(toastLiveExample)
+
+    toast.show()
+  })
+}
