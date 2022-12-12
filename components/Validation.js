6026d26549c67b0737ea247b520f1ae15c1f9c4c
Your Name <mehdy.badaoui07@gmail.com>
2022-12-12 17:40:43 +0100

    changed all

--- components/Validation.js
+++ components/Validation.js
@@ -1,54 +1,25 @@
- document.getElementById("form").addEventListener("submit", function(e) {
-     
-     	let erreur;
-     
-     	let inputs = this.getElementsByTagName("input");
-     
-         let select = this.getElementsByTagName("select")
-    	
-         for (let i = 0; i < inputs.length; i++) {
-     		console.log(inputs[i]);
-     		if (!inputs[i].value) {
-     			erreur = "Veuillez renseigner tous les champs";
-     		}
-             if (!select[i].value) {
-                 erreur = "Veuillez sélectionner une valeur";
-             }
-     	}
-     
-     	if (erreur) {
-     		e.preventDefault();
-     		document.getElementById("erreur").innerHTML = erreur;
-     		return false;
-     	} else {
-     		alert('Formulaire envoyé !');
-     	}
-     
-     
-     	let name = document.getElementById("name");
-         let date = document.getElementById("date")
-     	let lieu = document.getElementById("lieu");
-     	let thème = document.getElementById("thème");
-     	let tarif = document.getElementById("tarif");
-         let description = document.getElementById("description");
-        
-         if (!description.value) {
-             erreur = "Ce champ est obligatoire ";
-         }
-         if (!tarif.value) {
-     		erreur = "Doit être positif";
-     	}
-     	if (!thème.value) {
-     		erreur = "Ce champ est obligatoire ";
-     	}
-     	if (!lieu.value) {
-     		erreur = "Ce champ est obligatoire ";
-         }
-         if (!date.value) {
-             erreur = "Doit être égale ou supérieure à aujourd’hui ";
-         }
-     	if (!name.value) {
-     		erreur = "Ce champ est obligatoire "; 
-         }
-     })
+const form = document.querySelector("form");
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+	
+});
+
+const elements = form.elements;
+
+for (const element of elements) {
+   
+        element.addEventListener("invalid", (event) => {
+			
+			event.preventDefault();
+			element.classList.add("is-invalid");
+
+			
+		});
+	
+
+}
+
+	
+    console.log("implement form reset");
+    console.log("implement toaster");
 
--- components/date.js
+++ components/date.js
@@ -21,14 +21,11 @@ console.log(today);
 const date = document.getElementById("date");
 date.setAttribute("min", today);*/
 
-// import date ?
-
 let today = new Date();
 let dd = String(today.getDate()).padStart(2, '0');
 let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
 let yyyy = today.getFullYear();
 
-if(day)
 today = mm + '/' + dd + '/' + yyyy;
 console.log(today);
 
