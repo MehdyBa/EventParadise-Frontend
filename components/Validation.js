13d0895c655e798d00d14c95fec289af7457aa9e
Your Name <mehdy.badaoui07@gmail.com>
2022-12-12 13:59:01 +0100

    changed files

--- components/Validation.js
+++ components/Validation.js
@@ -1,20 +1,54 @@
+// document.getElementById("form").addEventListener("submit", function(e) {
+     
+//     	let erreur;
+     
+//     	let inputs = this.getElementsByTagName("input");
+     
+//         let select = this.getElementsByTagName("select")
+    	
+//         for (let i = 0; i < inputs.length; i++) {
+//     		console.log(inputs[i]);
+//     		if (!inputs[i].value) {
+//     			erreur = "Veuillez renseigner tous les champs";
+//     		}
+//             if (!select[i].value) {
+//                 erreur = "Veuillez sélectionner une valeur";
+//             }
+//     	}
+     
+//     	if (erreur) {
+//     		e.preventDefault();
+//     		document.getElementById("erreur").innerHTML = erreur;
+//     		return false;
+//     	} else {
+//     		alert('Formulaire envoyé !');
+//     	}
+     
+     
+//     	let name = document.getElementById("name");
+//         let date = document.getElementById("date")
+//     	let lieu = document.getElementById("lieu");
+//     	let thème = document.getElementById("thème");
+//     	let tarif = document.getElementById("tarif");
+//         let description = document.getElementById("description");
+        
+//         if (!description.value) {
+//             erreur = "Ce champ est obligatoire ";
+//         }
+//         if (!tarif.value) {
+//     		erreur = "Doit être positif";
+//     	}
+//     	if (!thème.value) {
+//     		erreur = "Ce champ est obligatoire ";
+//     	}
+//     	if (!lieu.value) {
+//     		erreur = "Ce champ est obligatoire ";
+//         }
+//         if (!date.value) {
+//             erreur = "Doit être égale ou supérieure à aujourd’hui ";
+//         }
+//     	if (!name.value) {
+//     		erreur = "Ce champ est obligatoire "; 
+//         }
+//     })
 
-/*const input = document.querySelector('input')
-const log = document.getElementById('log')
-
-input.addEventListener('invalid', (e) => {
-  log.appendChild(Object.assign(
-    document.createElement('li'),
-    { textContent: JSON.stringify(e.target.value) }
-  ))
-})*/
-
-const toastTrigger = document.getElementById('liveToastBtn')
-const toastLiveExample = document.getElementById('liveToast')
-if (toastTrigger) {
-  toastTrigger.addEventListener('click', () => {
-    const toast = new bootstrap.Toast(toastLiveExample)
-
-    toast.show()
-  })
-}
---
+++ components/date.js
@@ -0,0 +1,25 @@
+
+let today = new Date();
+let day = today.getDay();
+let month = today.getMonth()+1;
+const year = today.getFullYear();
+
+
+if( day < 10) {
+    day = "0" + day;
+}
+
+if (month < 10) {
+    month = "0" + month;
+}
+
+
+today = day + '/' + month + '/' + year;
+
+console.log(today);
+
+const date = document.getElementById("date");
+date.setAttribute("min", today);
+
+
+
